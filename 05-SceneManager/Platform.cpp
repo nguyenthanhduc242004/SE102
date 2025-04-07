f9c3731b3632fb748c767fb27e3cf544bd7c4d50
Duc <nguyenthanhduc242004@gmail.com>
2025-04-07 16:52:59 +0700

    Tạo platform cho map 1-1

--- 05-SceneManager/Platform.cpp
+++ 05-SceneManager/Platform.cpp
@@ -59,6 +59,7 @@ void CPlatform::GetBoundingBox(float& l, float& t, float& r, float& b)
 
 int CPlatform::IsDirectionColliable(float nx, float ny)
 {
+	if (nx == -1 || nx == 1) return 1;
 	if (nx == 0 && ny == -1) return 1;
 	else return 0;
 }
