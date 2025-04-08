43ff23e9371d5cdf6fa9dbecd866289e519a24f5
Phu Tho <phanphutho9999@gmail.com>
2025-04-09 00:12:32 +0700

    Refactor: Loading objects now can be done in iterations using vector container.

--- 05-SceneManager/PlayScene.cpp
+++ 05-SceneManager/PlayScene.cpp
@@ -14,7 +14,7 @@
 
 using namespace std;
 
-CPlayScene::CPlayScene(int id, LPCWSTR filePath):
+CPlayScene::CPlayScene(int id, LPCWSTR filePath) :
 	CScene(id, filePath)
 {
 	player = NULL;
@@ -49,7 +49,7 @@ void CPlayScene::_ParseSection_SPRITES(string line)
 	if (tex == NULL)
 	{
 		DebugOut(L"[ERROR] Texture ID %d not found!\n", texID);
-		return; 
+		return;
 	}
 
 	CSprites::GetInstance()->Add(ID, l, t, r, b, tex);
@@ -62,7 +62,7 @@ void CPlayScene::_ParseSection_ASSETS(string line)
 	if (tokens.size() < 1) return;
 
 	wstring path = ToWSTR(tokens[0]);
-	
+
 	LoadAssets(path.c_str());
 }
 
@@ -80,7 +80,7 @@ void CPlayScene::_ParseSection_ANIMATIONS(string line)
 	for (int i = 1; i < tokens.size(); i += 2)	// why i+=2 ?  sprite_id | frame_time  
 	{
 		int sprite_id = atoi(tokens[i].c_str());
-		int frame_time = atoi(tokens[i+1].c_str());
+		int frame_time = atoi(tokens[i + 1].c_str());
 		ani->Add(sprite_id, frame_time);
 	}
 
@@ -88,7 +88,7 @@ void CPlayScene::_ParseSection_ANIMATIONS(string line)
 }
 
 /*
-	Parse a line in section [OBJECTS] 
+	Parse a line in section [OBJECTS]
 */
 void CPlayScene::_ParseSection_OBJECTS(string line)
 {
@@ -101,28 +101,58 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 	float x = (float)atof(tokens[1].c_str());
 	float y = (float)atof(tokens[2].c_str());
 
-	CGameObject *obj = NULL;
+	//CGameObject *obj = NULL;
+	vector<LPGAMEOBJECT> objs;
 
 	switch (object_type)
 	{
 	case OBJECT_TYPE_MARIO:
-		if (player!=NULL) 
+		if (player != NULL)
 		{
 			DebugOut(L"[ERROR] MARIO object was created before!\n");
 			return;
 		}
-		obj = new CMario(x,y); 
-		player = (CMario*)obj;  
+		player = new CMario(x, y);
+		objs.push_back(player);
 
 		DebugOut(L"[INFO] Player object has been created!\n");
 		break;
-	case OBJECT_TYPE_GOOMBA: obj = new CGoomba(x,y); break;
-	case OBJECT_TYPE_BRICK: obj = new CBrick(x,y); break;
-	case OBJECT_TYPE_COIN: obj = new CCoin(x, y); break;
+	case OBJECT_TYPE_GOOMBA: objs.push_back(new CGoomba(x, y)); break;
+	case OBJECT_TYPE_BRICK:
+	{
+		// number of iterations into a maze
+		int itX = (int)atof(tokens[3].c_str());
+		int itY = (int)atof(tokens[4].c_str());
+
+		// distance between objects following directX top-left axis
+		int offsetX = (int)atof(tokens[5].c_str());
+		int offsetY = (int)atof(tokens[6].c_str());
+		for (int i = 0; i < itX; i++) {
+			for (int j = 0; j < itY; j++) {
+				objs.push_back(new CBrick(x + i * offsetX, y + j * offsetY));
+			}
+		}
+		break;
+	}
+	case OBJECT_TYPE_COIN:
+	{
+		// number of iterations into a maze
+		int itX = (int)atof(tokens[3].c_str());
+		int itY = (int)atof(tokens[4].c_str());
+
+		// distance between objects following directX top-left axis
+		int offsetX = (int)atof(tokens[5].c_str());
+		int offsetY = (int)atof(tokens[6].c_str());
+		for (int i = 0; i < itX; i++) {
+			for (int j = 0; j < itY; j++) {
+				objs.push_back(new CCoin(x + i * offsetX, y + j * offsetY));
+			}
+		}
+		break;
+	}
 
 	case OBJECT_TYPE_PLATFORM:
 	{
-
 		float cell_width = (float)atof(tokens[3].c_str());
 		float cell_height = (float)atof(tokens[4].c_str());
 		int length = atoi(tokens[5].c_str());
@@ -134,12 +164,12 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 		boolean direction3 = atoi(tokens[11].c_str());
 		boolean direction4 = atoi(tokens[12].c_str());
 
-		obj = new CPlatform(
+		objs.push_back(new CPlatform(
 			x, y,
 			cell_width, cell_height, length,
 			sprite_begin, sprite_middle, sprite_end,
 			direction1, direction2, direction3, direction4
-		);
+		));
 
 		break;
 	}
@@ -149,7 +179,7 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 		float r = (float)atof(tokens[3].c_str());
 		float b = (float)atof(tokens[4].c_str());
 		int scene_id = atoi(tokens[5].c_str());
-		obj = new CPortal(x, y, r, b, scene_id);
+		objs.push_back(new CPortal(x, y, r, b, scene_id));
 	}
 	break;
 
@@ -160,10 +190,11 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 	}
 
 	// General object setup
-	obj->SetPosition(x, y);
+	//obj->SetPosition(x, y);
 
-
-	objects.push_back(obj);
+	for (LPGAMEOBJECT obj : objs) {
+		objects.push_back(obj);
+	}
 }
 
 void CPlayScene::LoadAssets(LPCWSTR assetFile)
@@ -209,7 +240,7 @@ void CPlayScene::Load()
 	f.open(sceneFilePath);
 
 	// current resource section flag
-	int section = SCENE_SECTION_UNKNOWN;					
+	int section = SCENE_SECTION_UNKNOWN;
 
 	char str[MAX_SCENE_LINE];
 	while (f.getline(str, MAX_SCENE_LINE))
@@ -219,15 +250,15 @@ void CPlayScene::Load()
 		if (line[0] == '#') continue;	// skip comment lines	
 		if (line == "[ASSETS]") { section = SCENE_SECTION_ASSETS; continue; };
 		if (line == "[OBJECTS]") { section = SCENE_SECTION_OBJECTS; continue; };
-		if (line[0] == '[') { section = SCENE_SECTION_UNKNOWN; continue; }	
+		if (line[0] == '[') { section = SCENE_SECTION_UNKNOWN; continue; }
 
 		//
 		// data section
 		//
 		switch (section)
-		{ 
-			case SCENE_SECTION_ASSETS: _ParseSection_ASSETS(line); break;
-			case SCENE_SECTION_OBJECTS: _ParseSection_OBJECTS(line); break;
+		{
+		case SCENE_SECTION_ASSETS: _ParseSection_ASSETS(line); break;
+		case SCENE_SECTION_OBJECTS: _ParseSection_OBJECTS(line); break;
 		}
 	}
 
@@ -253,13 +284,13 @@ void CPlayScene::Update(DWORD dt)
 	}
 
 	// skip the rest if scene was already unloaded (Mario::Update might trigger PlayScene::Unload)
-	if (player == NULL) return; 
+	if (player == NULL) return;
 
 	// Update camera to follow mario
 	float cx, cy;
 	player->GetPosition(cx, cy);
 
-	CGame *game = CGame::GetInstance();
+	CGame* game = CGame::GetInstance();
 	cx -= game->GetBackBufferWidth() / 2;
 	cy -= game->GetBackBufferHeight() / 2;
 
@@ -292,7 +323,7 @@ void CPlayScene::Clear()
 /*
 	Unload scene
 
-	TODO: Beside objects, we need to clean up sprites, animations and textures as well 
+	TODO: Beside objects, we need to clean up sprites, animations and textures as well
 
 */
 void CPlayScene::Unload()
