2585b1630a502846e68de59ff4458aabd3c6f3da
Duc <nguyenthanhduc242004@gmail.com>
2025-05-26 18:58:16 +0700

    Turned of Tanookitail RenderBoundingBox() and commented unused methods.

--- 05-SceneManager/TanookiTail.h
+++ 05-SceneManager/TanookiTail.h
@@ -13,18 +13,18 @@ protected:
 	bool hasHit = false;
 
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
-	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	//virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void Render() {
-		RenderBoundingBox();
+		//RenderBoundingBox();
 	};
 
 	virtual int IsCollidable() { return 1; };
 	virtual int IsBlocking() { return 0; }
 
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
-	void HandleTimer(DWORD dt);
+	//void HandleTimer(DWORD dt);
 public:
 	CTanookiTail(float x, float y) : CGameObject(x, y) {}
-	void SetState(int state);
+	//void SetState(int state);
 };
 
